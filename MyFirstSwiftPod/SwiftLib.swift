1d16aface721721a6f046cde2111c8dfbf639cbd
anujmishra <anujkm@dewsolutions.in>
2022-09-09 13:54:21 +0530

    Test cases added

---
+++ MyFirstSwiftPod/SwiftLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftLib.swift
+//  MyFirstSwiftPod
+//
+//  Created by Anuj Mishra on 09/09/22.
+//
+
+import Foundation
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
