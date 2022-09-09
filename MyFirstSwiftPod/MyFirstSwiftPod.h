86959faad0ec149997a628d838849688d7bf6856
anujmishra <anujkm@dewsolutions.in>
2022-09-09 11:45:39 +0530

    Initial project setup

---
+++ MyFirstSwiftPod/MyFirstSwiftPod.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstSwiftPod.h
+//  MyFirstSwiftPod
+//
+//  Created by Anuj Mishra on 08/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstSwiftPod.
+FOUNDATION_EXPORT double MyFirstSwiftPodVersionNumber;
+
+//! Project version string for MyFirstSwiftPod.
+FOUNDATION_EXPORT const unsigned char MyFirstSwiftPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstSwiftPod/PublicHeader.h>
+
+
